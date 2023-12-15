bcef82f227fde3ed5c2ad4268a7fa9f0b431cfd0
nayandrakshapally <nayan.drakshapally@gmail.com>
2023-12-15 16:04:20 -0500

    Collections

---
+++ CollectionsT.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class CollectionsT {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        Collections.sort(nums);
+        System.out.println(nums);
+        System.out.println(nums.indexOf(2));
+        System.out.println(nums.get(1));
+
+        // set supports unique values, it doesn't have index values
+        Set<Integer> nums2 = new HashSet<>();
+        nums2.add(1);
+        nums2.add(2);
+        nums2.add(3);
+        System.out.println(nums2);
+
+        // Map it is the collection of key and value pairs
+        Map<String, Integer> student = new HashMap<>();
+        student.put("Nayan", 17);
+        student.put("test", 18);
+        System.out.println(student);
+        System.out.println(student.get("Nayan"));
+        System.out.println(student.keySet());
+        // Both hasmap & hashtable works same
+    }
+    //comparator & comparable
+}
